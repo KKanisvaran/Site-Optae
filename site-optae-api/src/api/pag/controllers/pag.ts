4ebf3299813981238e7bc0c84a5d58375ed851f6
KKanisvaran <kanisvaran06@gmail.com>
2026-03-02 11:19:18 +0100

    Initialisation de strapi + creation du doc site-optae-front

---
+++ site-optae-api/src/api/pag/controllers/pag.ts
@@ -0,0 +1,7 @@
+/**
+ * pag controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::pag.pag');
---
+++ site-optae-api/src/api/pag/services/pag.ts
@@ -0,0 +1,7 @@
+/**
+ * pag service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::pag.pag');
